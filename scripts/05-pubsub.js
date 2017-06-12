d3b98f5f9171af4b90402c796de70ea11b08d02b
JacopoDaeli <jacopo.daeli@gmail.com>
2017-06-11 18:17:20 -0700

    Update PubSub

--- scripts/05-pubsub.js
+++ scripts/05-pubsub.js
@@ -1,4 +1,20 @@
-// 04 - PubSub Pattern
+// 05 - PubSub Pattern
+
+// Whilst the Observer pattern is useful to be aware of, quite often
+// in the JavaScript world, we'll find it commonly implemented using
+// a variation known as the Publish/Subscribe pattern. Whilst very
+// similar, there are differences between these patterns worth noting.
+
+// The Observer pattern requires that the observer (or object) wishing to
+// receive topic notifications must subscribe this interest to the object
+// firing the event (the subject).
+
+// The Publish/Subscribe pattern however uses a topic/event channel which
+// sits between the objects wishing to receive notifications (subscribers)
+// and the object firing the event (the publisher). This event system allows
+// code to define application specific events which can pass custom arguments
+// containing values needed by the subscriber. The idea here is to avoid
+// dependencies between the subscriber and publisher.
 
 class PubSub {
   constructor () {
